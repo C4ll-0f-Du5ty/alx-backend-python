43f57f53436ce4b260f6a7c6a3b3953a52e16d69
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-14 21:40:02 +0300

    done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        list[float]: _description_
+    """
     delays = []
     for _ in range(n):
         bisect.insort(delays, await wait_random(max_delay))
