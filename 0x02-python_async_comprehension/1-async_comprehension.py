8793c4b6d6fe9a906e84c452bf0b311aeda39a74
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-10-15 22:50:21 +0300

    done

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,17 +6,15 @@
     """
 
 
-from typing import List
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> typing.List[float]:
     """_summary_
 
     Returns:
         typing.List[float]: _description_
     """
-    l: list = []
-    async for i in async_generator():
-        l.append(i)
+    l = [i async for i in async_comprehension()]
     return l
